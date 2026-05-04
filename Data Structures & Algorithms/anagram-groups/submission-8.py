3db21437a69cf0d953100f852f9131fe6154f35b
Charanvardhan Reddy Mannuru <94391149+Charanvardhan@users.noreply.github.com>
2026-05-03 21:53:48 -0400

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+
+def findKey(s):
+    key = [0] * 26
+    for k in s:
+        idx = ord(k) % 97
+        key[idx] += 1
+    return tuple(key)
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # if len(strs) == 0:
+        #     return [[]]
+        # groups = defaultdict(list)
+        groups = dict()
+        for i in strs:
+            key = findKey(i)
+            if key in groups:
+                groups[key].append(i)
+            else:
+                groups[key] = [i]
+            
+        return list(groups.values())
