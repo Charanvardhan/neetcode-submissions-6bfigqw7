a5ea92897096d386dcd7b1adbf99f665ec8d9b42
Charanvardhan Reddy Mannuru <94391149+Charanvardhan@users.noreply.github.com>
2026-05-20 22:03:23 -0400

    Add: find-median-in-a-data-stream - submission-2

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-2.py
@@ -0,0 +1,58 @@
+import heapq
+class MedianFinder:
+    def __init__(self):
+        self.lhp = []
+        self.rhp = []
+
+    def addNum(self, num: int) -> None:
+        lhp = self.lhp
+        rhp = self.rhp
+        ll = len(lhp)
+        rl = len(rhp)
+
+        # if ll == 0:
+        #     heapq.heappush(lhp, -num)
+        #     return
+        # if rl == 0:
+        #     if num < -lhp[0]:
+        #         temp = -heapq.heappop(lhp)
+        #         heapq.heappush(rhp, temp)
+        #         heapq.heappush(lhp, -num)
+        #     else:
+        #         heapq.heappush(rhp, num)
+        #     return
+
+        if ll == rl:
+            if ll == 0 or num <= -lhp[0]:
+                heapq.heappush(lhp, -num)
+            else:
+                heapq.heappush(rhp, num)
+        
+        elif ll > rl:
+            if num < -lhp[0]:
+                temp = -heapq.heappop(lhp)
+                heapq.heappush(rhp, temp)
+                heapq.heappush(lhp, -num)
+            else:
+                heapq.heappush(rhp, num)
+        else:
+            if num > rhp[0]:
+                temp = heapq.heappop(rhp)
+                heapq.heappush(rhp, num)
+                heapq.heappush(lhp, -temp)
+            else:
+                heapq.heappush(lhp, -num)
+        
+
+    def findMedian(self) -> float:
+        lhp = self.lhp
+        rhp = self.rhp
+        ll = len(lhp)
+        rl = len(rhp)
+
+        if ll == rl:
+            return (-lhp[0] + rhp[0]) / 2
+        elif ll > rl:
+            return float(-lhp[0])
+        else:
+            return float(rhp[0])
