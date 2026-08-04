f13ed398195b608fdc5ec402ef25d82b4e82790a
Charanvardhan Reddy Mannuru <94391149+Charanvardhan@users.noreply.github.com>
2026-08-03 21:16:57 -0400

    Add: islands-and-treasure - submission-1

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-1.py
@@ -0,0 +1,34 @@
+from collections import deque
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        rows, cols = len(grid), len(grid[0])
+        queue = deque()
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 0:
+                    queue.append([i,j, 0])
+        
+        helper(rows, cols, queue, grid)
+    
+
+def helper(rows, cols, queue, grid):
+
+    while len(queue) > 0:
+        row, col, dist = queue.popleft()
+
+        if row + 1 < rows and grid[row + 1][col] == 2147483647:
+            grid[row + 1][col] = dist+1
+            queue.append([row + 1, col, dist+1])
+
+        if row - 1 >= 0 and grid[row - 1][col] == 2147483647:
+            grid[row - 1][col] = dist+1
+            queue.append([row - 1, col, dist+1])
+        
+        if col + 1 < cols and grid[row][col + 1] == 2147483647:
+            grid[row][col + 1] = dist+1
+            queue.append([row, col + 1, dist+1])
+        
+        if col - 1 >= 0 and grid[row][col - 1] == 2147483647:
+            grid[row][col - 1] = dist+1
+            queue.append([row, col - 1, dist + 1])
