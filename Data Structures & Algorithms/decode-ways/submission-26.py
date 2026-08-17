2f9bfa6247ca9715f9384b652d1891aecf2ad7ea
Charanvardhan Reddy Mannuru <94391149+Charanvardhan@users.noreply.github.com>
2026-08-16 20:35:21 -0400

    Add: decode-ways - submission-26

---
+++ Data Structures & Algorithms/decode-ways/submission-26.py
@@ -0,0 +1,27 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n = len(s)
+        
+        mem = {}
+        
+        def traversal(i):
+            nonlocal mem
+
+            if i in mem:
+                return mem[i]
+            if i == n:
+                return 1
+            
+            if s[i] == '0':
+                return 0
+            
+            
+            temp = traversal(i+1)
+            if i+1 < n and 9 < int(s[i] + s[i+1]) < 27:
+                temp += traversal(i + 2)
+            
+            mem[i] = temp
+            return temp
+        
+
+        return traversal(0)
