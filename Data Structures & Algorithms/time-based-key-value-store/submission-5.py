3e5ac53f5dcf2e61d7390ac232840a44e0bacc81
Charanvardhan Reddy Mannuru <94391149+Charanvardhan@users.noreply.github.com>
2026-08-25 07:28:06 -0400

    Add: time-based-key-value-store - submission-5

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-5.py
@@ -0,0 +1,35 @@
+class TimeMap:
+
+    def __init__(self):
+        self.mapper = dict()
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key in self.mapper:
+            self.mapper[key].append([value, timestamp])
+        else:
+            self.mapper[key] = list()
+            self.mapper[key].append([value, timestamp])
+        
+
+    def get(self, key: str, timestamp: int) -> str:
+        if not key in self.mapper:
+            return ''
+        
+        values = self.mapper[key]
+
+        l = 0
+        r = len(values) - 1
+
+        while l <= r:
+            mid = (l+r) // 2
+            if values[mid][1] == timestamp:
+                return values[mid][0]
+            elif values[mid][1] > timestamp:
+                r = mid - 1
+            else:
+                l = mid + 1
+        if r < 0:
+            return ""
+        else:
+            return values[r][0]
